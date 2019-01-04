f5ed55b83d82d67123b4b99324fe57f29f6b4b40
zhangcunxia <252946169@qq.com>
2019-01-04 10:01:44 +0800

    添加splash库

--- LooksDelicious/android/app/src/main/java/com/looksdelicious/MainApplication.java
+++ LooksDelicious/android/app/src/main/java/com/looksdelicious/MainApplication.java
@@ -3,6 +3,7 @@ package com.looksdelicious;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new RNSpinkitPackage(),
             new RNGestureHandlerPackage()
       );
